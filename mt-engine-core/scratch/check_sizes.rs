76842f335d2cdedf20b4aa11af666fd9c5e047bc
devine <iamliuhongru@gmail.com>
2026-04-10 01:47:27 +0800

    chore: cleanup scratch files after verification

--- mt-engine-core/scratch/check_sizes.rs
+++
@@ -1,7 +0,0 @@
-use mt_engine_core::orders::OrderData;
-use mt_engine_core::engine::TriggerNode;
-
-fn main() {
-    println!("OrderData: {} bytes", std::mem::size_of::<OrderData>());
-    println!("TriggerNode: {} bytes", std::mem::size_of::<TriggerNode>());
-}
